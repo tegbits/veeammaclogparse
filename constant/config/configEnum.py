b3c6498d99dbfd161768829bde9cd49aa2adfb14
MainMi <miksmikita7@gmail.com>
2023-10-11 21:04:37 +0300

    hot fix name

--- constant/config/configEnum.py
+++ constant/config/configEnum.py
@@ -6,5 +6,5 @@ SECRET_EMAIL = os.getenv('NO_REPLY_EMAIL') or 'test@gmail.com'
 SECRET_PASS = os.getenv('NO_REPLY_PASS') or 'adfa fasf fasf fafs'
 SENDER_EMAIL = os.getenv('SEND_EMAIL').split(',') or 'test@gmail.com'
 TYPE_INFO = os.getenv('TYPE_INFO').split('+') if os.getenv('TYPE_INFO') else ['FULL']
-HTTP_HOST = os.getenv('HTTP_HOST') or 'smtp.gmail.com'
-HTTP_IP = os.getenv('HTTP_IP') or 465
+SMTP_HOST = os.getenv('SMTP_HOST') or 'smtp.gmail.com'
+SMTP_PORT = os.getenv('SMTP_PORT') or 465
--- sender.py
+++ sender.py
@@ -3,7 +3,7 @@ from email.message import EmailMessage
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from template import templateInfo
-from constant.config.configEnum import SECRET_EMAIL, SECRET_PASS, SENDER_EMAIL, HTTP_HOST, HTTP_IP
+from constant.config.configEnum import SECRET_EMAIL, SECRET_PASS, SENDER_EMAIL, SMTP_HOST, SMTP_PORT
 from constant.templateTypeEnum import JOB_TYPE
 
 # Create an instance of EmailMessage and MIMEMultipart
@@ -44,7 +44,7 @@ def sendDataInEmail(data, emails=SENDER_EMAIL, templateType = JOB_TYPE):
     context = ssl.create_default_context()
     
     # Connect to the SMTP server using SSL
-    with smtplib.SMTP_SSL(HTTP_HOST, HTTP_IP, context=context) as smtp:
+    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT, context=context) as smtp:
         try:
             # Log in to the SMTP server using the secret email and password
             smtp.login(SECRET_EMAIL, SECRET_PASS)
